dab8382bdadd8634137118b0770987397af0265d
Ifex <if3cofr3sh@gmail.com>
2020-11-19 18:16:09 +0100

    created events module ref

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const Person = require('./app');
+const Logger = require('./logger');
 
 const person1 = new Person('Ifex', 21);
 
 person1.greeting();
+
+const logger = new Logger();
+
+logger.on('message', (data) => console.log(`Called Listener`, data));
+
+logger.log('Hello World');
+logger.log('Whats up');
+logger.log('You good?');
