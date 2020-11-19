4e47c1973fe5c073c162f816bf4477e0fb3c7f55
Ifex <if3cofr3sh@gmail.com>
2020-11-19 19:16:26 +0100

    created a server and api

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const Person = require('./app');
-const Logger = require('./logger');
-
 const person1 = new Person('Ifex', 21);
 
-person1.greeting();
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
 
-const logger = new Logger();
+const server = http.createServer((req, res) => {
+	// console.log(req.url); // gets the url path
+	if (req.url === '/') {
+		fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, content) => {
+			if(err) throw err;
+			res.writeHead(200, { 'Content-Type': 'text/html'});
+			res.end(content);
+		})
+	} else if (req.url === '/about') {
+		fs.readFile(path.join(__dirname, 'public', 'about.html'), (err, content) => {
+			if(err) throw err;
+			res.writeHead(200, { 'Content-Type': 'text/html'});
+			res.end(content);
+		})
+	} else if (req.url === '/api/users') {
+		const users = [
+		{name: 'James Philip', age: 40 },
+		{name: 'Paul White', age: 32 },
+		{name: 'Ray Brown', age: 28 },
+		{name: 'Marvis Dan', age: 37 },
+		{name: 'Bob Smith', age: 26 }
+		];
+		res.writeHead(200, { 'Content-Type': 'application/json' });
+		res.end(JSON.stringify(users));
+	}
+});
 
-logger.on('message', (data) => console.log(`Called Listener`, data));
+const PORT = process.env.PORT || 5000;
 
-logger.log('Hello World');
-logger.log('Whats up');
-logger.log('You good?');
+
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
--- logger.js
+++ logger.js
@@ -8,4 +8,12 @@ class Logger extends EventEmitter {
 	}
 }
 
-module.exports = Logger;
+const logger = new Logger();
+
+logger.on('message', (data) => console.log(`Called Listener`, data));
+
+logger.log('Hello World');
+logger.log('Whats up');
+logger.log('You good?');
+
+// module.exports = Logger;
